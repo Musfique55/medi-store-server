43314a16574b03b0a9cf465ac937cf40eab94cc6
Mufique Patwary <mdmusfiquepatwary@gmail.com>
2026-04-26 19:20:33 +0600

    fixed package.json

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 const server = async() => {
     try {
         await prisma.$connect();
-        console.log("server is connected");
         app.listen(PORT,() => {
             console.log("server is running on",PORT);
         })
